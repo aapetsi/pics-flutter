db507c3181dbdb8032aea386ea89595279f84b66
aapetsi <apetsiampiah@gmail.com>
2020-06-22 16:31:32 +0000

    add app class component

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
+import 'src/app.dart';
 
 void main() {
-  var app = MaterialApp(
-    home: Text('Hi there!'),
-  );
+  var app = App();
 
   runApp(app);
 }
