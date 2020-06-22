c90d358014078d5566b110e0c9c90ec5355a6772
aapetsi <apetsiampiah@gmail.com>
2020-06-22 03:59:04 +0000

    remove not needed comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-// import helper library from flutter to get content on screen
 import 'package:flutter/material.dart';
 
-// define a main function to run when the app starts
 void main() {
   var app = MaterialApp(
     home: Text('Hi there!'),
